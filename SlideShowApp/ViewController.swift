50f7580a9a650a915af7c2f51b743fc1b568aa44
KaitoKashiwazaki <kahiwazakikato45@gmail.com>
2022-08-22 20:00:18 +0900

    Initial Commit

---
+++ SlideShowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideShowApp
+//
+//  Created by 柏崎快音 on 2022/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
